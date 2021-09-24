b9abb98d54d63b88e0c64e454bfa6e5d4b2a5a21
Roman Shprenger <shprengerroman@gmail.com>
2021-09-24 23:07:26 +0300

    Edit list of sports in filter

--- src/components/Filters/FilterSport/FilterSport.js
+++ src/components/Filters/FilterSport/FilterSport.js
@@ -15,26 +15,28 @@ const FilterSport = () => {
     );
 
     setCheckedState(updatedCheckedState);
-
     setTotal(updatedCheckedState.filter(st => st).length);
   };
 
   const content = () => (
     <div className="filters__item-popover">
-      <ul className="filter__sport-list"></ul>
+      <ul className="filter__sport-list">
       {
         types.map((type, index) => <li className="filter__sport-item" key={type.id}>
+          <img src={`/sport-icons/${type.name}.svg`} alt={type.title} className="filter__sport-item-img" width="24" height="24" />
+          <label htmlFor={`custom-checkbox-${type.id}`} className="filter__sport-item-title">{type.title}</label>
           <input
             type="checkbox"
+            className="filter__sport-item-checkbox"
             id={`custom-checkbox-${type.id}`}
             name={type.name}
             value={type.name}
             checked={checkedState[index]}
             onChange={() => handleOnChange(index)}
           />
-          <label htmlFor={`custom-checkbox-${index}`}>{type.title}</label>
         </li>)
       }
+      </ul>
     </div>
   )
 
