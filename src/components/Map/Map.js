d0d7e19aa20f9d72be11a551f49c125da65bb4ad
Dmitry Dyakonov <d.v.dyakonov@gmail.com>
2021-10-23 18:38:07 +0300

    added density map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,15 +3,16 @@ import { useSelector } from 'react-redux';
 import {
   selectPoints,
 } from 'services/points/pointsSlice';
+import densityPoints from 'config/density.json';
 import { withYandexMap } from 'hocs';
 import './Map.scss';
 
-const init = (features, map) => {
+const init = (features, density, map) => {
   const ymaps = window.ymaps;
   const myMap = new ymaps.Map(map.current, {
     center: [55.76, 37.64],
     zoom: 10,
-    controls: ['zoomControl', 'typeSelector',  'fullscreenControl'],
+    controls: ['zoomControl', 'typeSelector', 'fullscreenControl'],
   }, {
     searchControlProvider: 'yandex#search'
   });
@@ -26,8 +27,11 @@ const init = (features, map) => {
 
   // Чтобы задать опции одиночным объектам и кластерам,
   // обратимся к дочерним коллекциям ObjectManager.
-  objectManager.objects.options.set('preset', 'islands#greenDotIcon');
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+  // objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+  // objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+
+
+  // objectManager.add(features);
   myMap.geoObjects.add(objectManager);
 
   const gradients = [{
@@ -37,15 +41,18 @@ const init = (features, map) => {
     .6: "rgba(255, 165, 0, 0.8)",
     .8: "rgba(234, 72, 58, 0.8)",
     1: "rgba(162, 36, 25, 0.8)"
-  },{
-    0.1: 'rgba(55, 255, 0, 0.7)',
-    0.2: 'rgba(55, 255, 0, 0.8)',
-    0.7: 'rgba(55, 72, 58, 0.9)',
-    1.0: 'rgba(55, 36, 25, 1)'
+  }, {
+    .1: "rgba(0, 255, 0, 0.5)",
+    .2: "rgba(173, 255, 47, 0.5)",
+    .4: "rgba(255, 255, 0, 0.8)",
+    .6: "rgba(255, 165, 0, 0.8)",
+    .8: "rgba(234, 72, 58, 0.8)",
+    1: "rgba(162, 36, 25, 0.8)"
   }
   ]
   const radiuses = [5, 10, 20, 30];
   const opacities = [0.4, 0.6, 0.8, 1];
+
 
   ymaps.modules.require(['Heatmap'], function (Heatmap) {
     var heatmapSport = new Heatmap(features, {
@@ -54,20 +61,54 @@ const init = (features, map) => {
       opacity: opacities[2]
     });
 
-    var heatmapDensity = new Heatmap(features, {
-      gradient: gradients[1],
-      radius: radiuses[1],
-      opacity: opacities[2]
+    var heatmapDensity = new Heatmap(density, {
+      radius: 15,
+      dissipating: !0,
+      intensityOfMidpoint: .01,
+      gradient: {
+        .1: "rgba(0, 255, 0, 0.5)",
+        .2: "rgba(173, 255, 47, 0.5)",
+        .4: "rgba(255, 255, 0, 0.8)",
+        .6: "rgba(255, 165, 0, 0.8)",
+        .8: "rgba(234, 72, 58, 0.8)",
+        1: "rgba(162, 36, 25, 0.8)"
+      }
     });
 
-      // Создадим переключатель вида подписей.
+    myMap.events.add("actionend", (function () {
+      switch (myMap.action.getCurrentState().zoom) {
+        case 11:
+          heatmapDensity.options.set("intensityOfMidpoint", .015);
+          break;
+        case 12:
+          heatmapDensity.options.set("intensityOfMidpoint", .03);
+          break;
+        case 13:
+          heatmapDensity.options.set("intensityOfMidpoint", .06);
+          break;
+        case 14:
+          heatmapDensity.options.set("intensityOfMidpoint", .15);
+          break;
+        case 15:
+          heatmapDensity.options.set("intensityOfMidpoint", .3);
+          break;
+        case 16:
+          heatmapDensity.options.set("intensityOfMidpoint", .5);
+          break;
+        default: 
+          heatmapDensity.options.set("intensityOfMidpoint", .005);
+          break;
+      }
+    }));
+
+    // Создадим переключатель вида подписей.
     var typeList = new ymaps.control.ListBox({
       data: {
-          content: 'Тепловая карта'
+        content: 'Тепловая карта'
       },
       items: [
-          new ymaps.control.ListBoxItem({data: {content: 'Спортивные объекты'}}),
-          new ymaps.control.ListBoxItem({data: {content: 'Население Москвы'}})
+        new ymaps.control.ListBoxItem({ data: { content: 'Спортивные объекты' } }),
+        new ymaps.control.ListBoxItem({ data: { content: 'Население Москвы' } })
       ]
     });
 
@@ -93,13 +134,13 @@ const init = (features, map) => {
       typeList.collapse();
     });
 
-    myMap.controls.add(typeList, {floatIndex: 0})
+    myMap.controls.add(typeList, { floatIndex: 0 })
   })
 }
 
 const Map = ({ isYmapsInit, mapData }) => {
   const points = useSelector(selectPoints);
-  const featuresFunc = (points) => ({
+  const sportPointsConversion = (points) => ({
     "type": "FeatureCollection",
     "features": points.map(point => (
       {
@@ -110,25 +151,46 @@ const Map = ({ isYmapsInit, mapData }) => {
           "coordinates": point.coords
         },
         "properties": {
+          "radius": point.radius,
           "balloonContentHeader": "<font size=3><b><a target='_blank' href='https://yandex.ru'>Здесь может быть ваша ссылка</a></b></font>", "balloonContentBody": "<p>Ваше имя: <input name='login'></p><p><em>Телефон в формате 2xxx-xxx:</em>  <input></p><p><input type='submit' value='Отправить'></p>", "balloonContentFooter": "<font size=1>Информация предоставлена: </font> <strong>этим балуном</strong>", "clusterCaption": "<strong><s>Еще</s> одна</strong> метка", "hintContent": "<strong>Текст  <s>подсказки</s></strong>"
         }
       }
     ))
   });
 
-  const [features, setFeatures] = useState(() => featuresFunc(points));
+  const densityPointsConversion = (points) => ({
+    "type": "FeatureCollection",
+    "features": points.map(point => (
+      {
+        "type": "Feature",
+        "id": point.id,
+        "geometry": {
+          "type": "Point",
+          "coordinates": [Number(point.lat), Number(point.lon)]
+        },
+        "properties": {
+          "weight": Number(point.weight),
+          "residents": Number(point.weight) * 31
+        }
+      }
+    ))
+  });
+
+  const [sportFeatures, setSportFeatures] = useState(() => sportPointsConversion(points));
+  const [densityFeatures, setDensityFeatures] = useState(() => densityPointsConversion(densityPoints));
   const mapRef = useRef(null);
   const ymaps = window.ymaps;
 
   useEffect(() => {
-    setFeatures(() => featuresFunc(points))
-  }, [points]);
+    setSportFeatures(() => sportPointsConversion(points))
+    setDensityFeatures(() => densityPointsConversion(densityPoints))
+  }, [points, densityPoints]);
 
   useEffect(() => {
     if (isYmapsInit) {
-      ymaps.ready(() => init(features, mapRef));
+      ymaps.ready(() => init(sportFeatures, densityFeatures, mapRef));
     }
-  }, [isYmapsInit, features, ymaps]);
+  }, [isYmapsInit, sportFeatures, densityFeatures, ymaps]);
 
   return (
     <div className="map" ref={mapRef}></div>
