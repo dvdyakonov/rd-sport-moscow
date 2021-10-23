3cdc0f2dd8836a3b62a8033f3e29ca35d6c1e536
Roman Shprenger <shprengerroman@gmail.com>
2021-10-23 23:27:33 +0300

    #9: Changed long select list

--- src/components/Filters/FilterObjects/FilterObjects.js
+++ src/components/Filters/FilterObjects/FilterObjects.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Select from 'react-select';
+import React, { useState } from 'react';
+import Select from "react-select";
 import { useSelector, useDispatch } from 'react-redux';
 import { selectFilters, setFilter } from 'services/points/pointsSlice';
 
@@ -7,21 +7,29 @@ import objects from 'config/objects.json';
 
 const FilterObjects = () => {
   const { depart } = useSelector(selectFilters);
+  const [options, setOptions] = useState(objects.slice(0,100));
   const dispatch = useDispatch();
   const handleOnChange = (val) => {
     dispatch(setFilter({
       param: 'depart',
-      value: val ? val.value : null
+      value: val ? val.value : ''
     }));
   };
 
-  return <div className="filters__item filter__avaliable">
+  const inputChange = (val) => {
+    const temp = objects.filter(item => item.label.indexOf(val) >= 0);
+    setOptions(temp.slice(0,100));
+  }
+
+  return <div className="filters__item filter__depart">
     <Select
+      name="depart"
+      isSearchable
       isClearable
-      name="avaliable"
-      options={objects}
       placeholder="По ведомствам"
       className="filter__field filter__field--select"
+      options={options}
+      onInputChange={inputChange}
       onChange={handleOnChange}
       value={objects.filter(el => el.value === depart)[0]}
     />
