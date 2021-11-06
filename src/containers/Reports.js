db52236790c6e8fea8f29de91d66e8957729137e
Roman Shprenger <shprengerroman@gmail.com>
2021-11-06 10:30:04 +0300

    Added link to table

--- src/containers/Reports.js
+++ src/containers/Reports.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import { useHistory } from "react-router-dom";
 import { useTable } from 'react-table'
 import Button from 'components/Button';
 import { exportToCsv, prepareCSVName, prepareCSV } from 'utils/csv';
@@ -10,19 +11,29 @@ const Reports = () => {
     console.log(prepareCSVName);
   };
 
+  const history = useHistory();
+
+  const goToPolygon = (id) => history.push(`/polygon/${id}`);
+
   const data = useMemo(
    () => [
      {
-       col1: 'Hello',
+       col1: '#0',
        col2: 'World',
+       col3: 'World',
+       col10: <Button className="reports__table-btn" size="s" kind="primary" onClick={() => goToPolygon(0)}>Инфо</Button>
      },
      {
-       col1: 'react-table',
-       col2: 'rocks',
+       col1: '#1',
+       col2: 'World',
+       col3: 'World',
+       col10: <Button className="reports__table-btn" size="s" kind="primary" onClick={() => goToPolygon(1)}>Инфо</Button>
      },
      {
-       col1: 'whatever',
-       col2: 'you want',
+       col1: '#2',
+       col2: 'World',
+       col3: 'World',
+       col10: <Button className="reports__table-btn" size="s" kind="primary" onClick={() => goToPolygon(2)}>Инфо</Button>
      },
    ],
    []
@@ -31,40 +42,44 @@ const Reports = () => {
  const columns = useMemo(
    () => [
      {
-       Header: 'Площадь области',
+       Header: 'ID',
        accessor: 'col1',
      },
      {
-       Header: 'Численность населения',
+       Header: 'Площадь',
        accessor: 'col2',
      },
      {
-       Header: 'Плотность населения на 1 км²',
+       Header: 'Население',
        accessor: 'col3',
      },
      {
-       Header: 'Количество спортивных зон',
+       Header: 'Плотность населения на 1 км²',
        accessor: 'col4',
      },
      {
-       Header: 'Количество спортивных зон на 1 человека',
+       Header: 'Кол-во спортзон',
        accessor: 'col5',
      },
      {
-       Header: 'Виды спорта',
+       Header: 'Кол-во спортзон на 1 человека',
        accessor: 'col6',
      },
      {
-       Header: 'Площадь спортивных зон',
+       Header: 'Виды спорта',
        accessor: 'col7',
      },
      {
-       Header: 'Площадь спортивных зон на 1 человека',
+       Header: 'Площадь спортзон',
        accessor: 'col8',
      },
      {
-       Header: 'Действия',
+       Header: 'Площадь спортзон на 1 человека',
        accessor: 'col9',
+     },
+     {
+       Header: 'Действия',
+       accessor: 'col10',
      }
    ],
    []
@@ -84,9 +99,9 @@ const Reports = () => {
         <div className="reports__container">
           <div className="reports__heading">
             <div className="reports__title">Отчеты</div>
-            <Button className="reporst__heading-action" onClick={saveFile}>Экспорт</Button>
+            <Button className="reports__heading-action" onClick={saveFile}>Экспорт</Button>
           </div>
-          <div className="reports__subtitle">Ниже представлен список сохраненных полигонов с информацией о спортивных объектах, попавших в его пределы</div>
+          <div className="reports__subtitle">Ниже представлен список сохраненных полигонов с информацией о спортивных зонах на его территории</div>
 
           <div className="reports__table-wrapper">
             <table {...getTableProps()} className="reports__table">
